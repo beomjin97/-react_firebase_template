8153f73b8aa676220118f579d9488b3114a07691
beomjin97 <qjawls646@gmail.com>
2023-11-03 12:57:48 +0900

    [FIX] does not exist on type 'Matcher<any>'

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,6 +4,10 @@ import App from "../App";
 describe("App", () => {
   it("간단 테스트", async () => {
     render(<App />);
-    expect(screen.getByText("react + firebase")).toBeDefined();
+    const heading = screen.getByRole("heading", {
+      name: "NextJS + Prisma",
+    });
+
+    expect(heading).toBeInTheDocument();
   });
 });
